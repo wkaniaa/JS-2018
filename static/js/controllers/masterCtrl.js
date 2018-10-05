e141f6e9bd7c10354e72f848db293cdde1f2f5f5
WSIZ <teamWSIZ@users.noreply.github.com>
2018-10-05 10:33:58 -0700

    Update masterCtrl.js

--- static/js/controllers/masterCtrl.js
+++ static/js/controllers/masterCtrl.js
@@ -5,12 +5,14 @@ angular.module('myApp.controllers',[]);
 angular.module('myApp.controllers').controller('masterCtrl',
     ['$rootScope','$scope', '$http', '$window', '$log',
         function ($rootScope, $scope, $http, $window, $log) {
+            /////////////////////////////////////////////////////////////
             $scope.M = {};
             $scope.M.results = [];
             $scope.search = {};
+            $scope.iloraz = 0;
 
-
-
+            $scope.AABB = 12; //to jest liczba
+            $scope.nazwaArtykulu = 'BREAKING NEWS'; //to jest napis
 
             $scope.callSomeHttp = function(){
                 return $http({
@@ -22,10 +24,17 @@ angular.module('myApp.controllers').controller('masterCtrl',
                 });
             };
 
+            $scope.wyliczIloraz = function(xxx, yyy) {
+                $scope.iloraz = xxx / yyy;
+            };
+
+
             ////// INIT STATE
 
             $scope.M.results = [
                 {imie:'Ariel', nazwisko:'Wrong', score:11}];
+
+            /////////////////////////////////////////////////////////////
 
         }
     ]
