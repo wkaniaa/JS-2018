d2dedd32d68dc11898c4134d4f9a78328ab1fc99
P Marecki <pmarecki.pro@gmail.com>
2018-10-26 19:29:49 +0200

    trójkąty

--- static/js/controllers/secondCtrl.js
+++ static/js/controllers/secondCtrl.js
@@ -5,6 +5,29 @@ angular.module('myApp.controllers').controller('secondCtrl',
             console.log('Loading second controller');
             $scope.M = {};
 
+            $scope.M.bokA = 0;
+            $scope.M.bokB = 0;
+            $scope.M.bokC = 0;
+
+            $scope.sprawdzTrojkat = function() {
+
+                // == oznacza sprawdzenie czy lewa storna równa się prawej
+                if ($scope.M.bokA==0) {
+                    $scope.M.wynik = 'Bok A jest 0 -- trójkąt nie istnieje!';
+                    return;
+                }
+                if ($scope.M.bokB==0) {
+                    $scope.M.wynik = 'Bok B jest 0 -- trójkąt nie istnieje!';
+                    return;
+                }
+                // A + B > C
+                // A + C > B
+                // B + C > A
+
+
+                $scope.M.wynik = 'OK -- trójkąt istnieje';
+            };
+
 
         }
     ]
