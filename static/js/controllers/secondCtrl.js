86fb456d24d2390d96e84de43791d3023f6ea361
P Marecki <pmarecki.pro@gmail.com>
2018-11-09 19:29:42 +0100

    pokaż/urkyj + tablice

--- static/js/controllers/secondCtrl.js
+++ static/js/controllers/secondCtrl.js
@@ -4,10 +4,11 @@ angular.module('myApp.controllers').controller('secondCtrl',
             /////////////////////////////////////////////////////////////
             console.log('Loading second controller');
             $scope.M = {};
-
             $scope.M.bokA = 0;
             $scope.M.bokB = 0;
             $scope.M.bokC = 0;
+            $scope.isVisible = true;
+            $scope.napis_wynik = '';
 
             $scope.sprawdzTrojkat = function() {
                 // $scope.xxx = (Math.random() * 10);
@@ -54,6 +55,64 @@ angular.module('myApp.controllers').controller('secondCtrl',
                 }
             };
 
+
+            $scope.potnij = function (tekst_napisu) {
+                console.log('Napis to: ' + tekst_napisu);
+                let tablica = tekst_napisu.split(',');
+                let inna_tablica = ['a','aa','b'];
+
+                console.log('Tablica to:' + JSON.stringify(tablica));
+                console.log('Element 0 to:' + tablica[0]);
+                console.log('Element 1 to:' + tablica[1]);
+                console.log('Element 2 to:' + tablica[2]);
+                tablica[2] = 'dwa';
+                console.log('Element 2 to:' + tablica[2]);
+
+                console.log('Elementów w tablicy:' + tablica.length);
+
+                tablica.sort();
+                $scope.napis_wynik = tablica;
+
+            };
+
+
+            $scope.potnijX = function (tekst) {
+                console.log('Do pocięcia: ' + tekst);
+                let w = tekst.split(",");
+                w.sort();
+                $scope.napis_wynik = w; //wszystkie elementy
+                console.log(w.length);
+                let ww = Array(100).fill(0);
+                $scope.napis_wynik = ww;
+
+                //podwójna tablica
+                let rows = tekst.split(":");
+                console.log('----');
+
+                for(let r of rows) {
+                    console.log(r);
+                }
+
+                let s = [];
+                s['a'] = 11;
+                console.log('>>' + s['a']);
+
+                let g = 'aa s r aww d a w ddd c a aa aa';
+                let gg = g.split(' ');
+                console.log(gg);
+                let mu = [];
+                let mx = 0;
+                let mm = '';
+                for(let gx of gg) {
+                    if (mu[gx]==undefined) mu[gx]=0;
+                    mu[gx]++;
+                    if (mu[gx]>mx) {
+                        mx = mu[gx];
+                        mm = gx;
+                    }
+                }
+                console.log('Word: ' + mm + ' count:' + mu[mm]);
+            }
 
         }
     ]
