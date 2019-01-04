9cd9e3c94c88bba77d0fbb5226cab44d0a3d08b4
pmarecki <pmarecki@gmail.com>
2019-01-04 19:28:36 +0100

    po zajeciach

--- static/js/controllers/dochodyCtrl.js
+++ static/js/controllers/dochodyCtrl.js
@@ -4,16 +4,27 @@ angular.module('myApp.controllers').controller('dochodyCtrl',
             /////////////////////////////////////////////////////////////
             console.log('Loading `dochody` controller');
             $scope.M = {};
+            $scope.wynik = [];
             $scope.dane = [
                 {pesel: 'AA11', name: 'Koepka', dochod: 413},
                 {pesel: 'VA11', name: 'Rose', dochod: 461},
                 {pesel: 'VA11', name: 'Johnson', dochod: 362},
                 {pesel: 'VA11', name: 'Thomas', dochod: 377}
-            ]
+            ];
 
 
             /////////////////////////////////////////////////////////////
 
+            $scope.wyszukaj = function () {
+                $scope.wynik = [];
+                for(let player of $scope.dane) {
+                    if ($scope.iLow !== undefined) {    //sprawdzenie czy coś zostało wpisane w pole iLow na UI
+                        if (player.dochod>= $scope.iLow) {
+                            $scope.wynik.push(player);
+                        }
+                    }
+                }
+            }
 
         }
     ]
